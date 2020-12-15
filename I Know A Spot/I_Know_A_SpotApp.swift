740f41375bfe7707e4248bd17b22bdc826e25978
Nathan Davenport <nathandaven@gmail.com>
2020-12-15 16:39:19 -0500

    Initial Commit

---
+++ I Know A Spot/I_Know_A_SpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Know_A_SpotApp.swift
+//  I Know A Spot
+//
+//  Created by Nathan Davenport on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct I_Know_A_SpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
